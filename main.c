8d64a3f64cc9830a5fc229a8a5a76a16a6b8194e
minju00-sm <m2nzoo@sookmyung.ac.kr>
2025-11-13 15:43:46 +0900

    11_6

--- main.c
+++ main.c
@@ -5,21 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	int grade[5];
-	int sum=0;
+	int i = 100;
+	int *p = &i;
+	int **q = &p;
 	
-	for (i=0; i<5; i++){
-		printf("grade[%i] = ", i);
-		scanf("%d", &grade[i]);
-	}
+	*p = 200;
+	printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
 	
-	for (i=0; i<5; i++){
-		printf("grade[%i] = %i\n", i, *(grade+i));
-		sum = sum + *(grade+i);
-	}
-	
-	printf("average: %i\n", sum/5);
+	**q = 300;
+	printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
 	
 	return 0;
 }
